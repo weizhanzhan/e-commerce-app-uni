a99c402c60ef9a88a378f61044be970e7ad92e8a
weizhanzhan <375786117@qq.com>
2020-06-02 14:29:00 +0800

    'index'

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,33 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		<view class="status_bar">
+			  <!-- 这里是状态栏 -->
 		</view>
+		<view class="home_header">
+			<view class="header_bar">
+				<view>京东</view>
+				<view>
+					<icon type="download" size="16"/>
+					<icon type="search" size="16"/>
+				</view>
+			</view>
+			<view class="header_search">
+				<view class="search_box">
+					<icon type="search" ></icon>
+					<input type="text" value="" />
+					<icon type="cancel" ></icon>
+				</view>
+			</view>
+			<view class="header_empty">
+				<view class="empty_box">
+					
+				</view>
+				<view class="empty_swiper">
+					1212
+				</view>
+			</view>
+		</view>
+		
 	</view>
 </template>
 
@@ -24,29 +48,45 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.status_bar {
+	height: var(--status-bar-height);
+	width: 100%;
+	background-color: #C92519;
+}
+.home_header{
+	background-color: #C92519;
+}
+.header_bar{
+	display: flex;
+	justify-content: space-between;
+}
+.header_search{
+	padding: 10px 0;
+}
+.search_box{
+	height: 30px;
+	width: 300px;
+	padding: 0 10px;
+	background-color: #FFFFFF;
+	border-radius: 15px;
+	margin: 0 auto;
+	display: flex;
+	align-items: center;
+}
+.header_empty{
+	background-color: #FFFFFF;
+	position: relative;
+}
+.empty_box{
+	background-color: #C92519;
+	height: 100px;
+}
+.empty_swiper{
+	height: 200px;
+	background-color: #eeeeee;
+	margin:  0 20px;
+	width: 100%;
+	position: absolute;
+	top: 0;
+}
 </style>
